4516db68abe2a3d5e1588f43252f9fc6d2a505e5
Marcio Jose Lima de Sousa <sotreze@yahoo.com.br>
2019-05-22 14:54:24 -0300

    CAMP - 07 04 Um pequeno desafio!

---
+++ financas/src/br/com/caelum/financas/teste/TesteMovimentacoesPorCategoria.java
@@ -0,0 +1,43 @@
+package br.com.caelum.financas.teste;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import br.com.caelum.financas.modelo.Categoria;
+import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.modelo.Movimentacao;
+import br.com.caelum.financas.modelo.TipoMovimentacao;
+import br.com.caelum.financas.util.JPAUtil;
+
+public class TesteMovimentacoesPorCategoria {
+	
+	public static void main(String[] args) {
+		
+		EntityManager em = new JPAUtil().getEntityManager();
+		em.getTransaction().begin();
+		
+		Categoria categoria = new Categoria();
+		categoria.setId(5);
+		
+		
+		String jpql = "select m from Movimentacao m join m.categoria c where c = :pCategoria";
+			
+		Query query = em.createQuery(jpql);
+		query.setParameter("pCategoria", categoria);
+		
+		
+		List<Movimentacao> resultados = query.getResultList();
+		
+		for(Movimentacao movimentacao : resultados) {
+			System.out.println("Descricao: " + movimentacao.getDescricao());
+			System.out.println("Conta.id: " +movimentacao.getId());
+		}
+		
+		em.getTransaction().commit();
+		em.close();
+		
+	}
+
+}
