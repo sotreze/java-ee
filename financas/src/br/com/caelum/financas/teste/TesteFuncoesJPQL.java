81210f1279ade6805ecf96fe2666adf7a556cf5c
Marcio Jose Lima de Sousa <sotreze@yahoo.com.br>
2019-05-27 11:32:21 -0300

    CAMP - 08 02 Funções JPQL

---
+++ financas/src/br/com/caelum/financas/teste/TesteFuncoesJPQL.java
@@ -0,0 +1,44 @@
+package br.com.caelum.financas.teste;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.modelo.Movimentacao;
+import br.com.caelum.financas.modelo.TipoMovimentacao;
+import br.com.caelum.financas.util.JPAUtil;
+
+public class TesteFuncoesJPQL {
+	
+	public static void main(String[] args) {
+		
+		
+		EntityManager em = new JPAUtil().getEntityManager();
+		em.getTransaction().begin();
+		
+		Conta conta = new Conta();
+		conta.setId(2);
+		
+		
+		String jpql = "select avg(m.valor) from Movimentacao m where m.conta = :pConta" +
+		" and m.tipo = :pTipo" +
+		" order by m.valor desc";
+		
+		Query query = em.createQuery(jpql);
+		query.setParameter("pConta", conta);
+		query.setParameter("pTipo", TipoMovimentacao.ENTRADA);
+		
+		Double media = (Double) query.getSingleResult();
+		
+		System.out.println("A média é: " + media);
+		
+		em.getTransaction().commit();
+		em.close();
+		
+	}
+	
+
+}
