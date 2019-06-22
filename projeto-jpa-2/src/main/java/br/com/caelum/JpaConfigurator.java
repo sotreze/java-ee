d6f7baccf14d3ba5cef735505adb104b532e0949
Marcio Sousa <sotreze@yahoo.com.br>
2019-06-21 23:15:37 -0300

    Home - Conhecendo o cache de primeiro nível

--- projeto-jpa-2/src/main/java/br/com/caelum/JpaConfigurator.java
+++ projeto-jpa-2/src/main/java/br/com/caelum/JpaConfigurator.java
@@ -49,7 +49,7 @@ public class JpaConfigurator {
 
 		props.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
 		props.setProperty("hibernate.show_sql", "true");
-		props.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+		props.setProperty("hibernate.hbm2ddl.auto", "update");
 
 		entityManagerFactory.setJpaProperties(props);
 		return entityManagerFactory;
