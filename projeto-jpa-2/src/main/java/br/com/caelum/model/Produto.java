1e849c8873e356947f7449fd21ffa3f385220b46
Marcio Jose Lima de Sousa <sotreze@yahoo.com.br>
2019-06-24 15:28:42 -0300

    CAMP - Melhorando o desempenho com Cache

--- projeto-jpa-2/src/main/java/br/com/caelum/model/Produto.java
+++ projeto-jpa-2/src/main/java/br/com/caelum/model/Produto.java
@@ -15,9 +15,12 @@ import javax.persistence.Version;
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
 
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
+@Cache(usage=CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
 public class Produto {
 
 	@Id
@@ -39,6 +42,7 @@ public class Produto {
 	private double preco;
 	
 	@ManyToMany
+	@Cache(usage=CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
 	private List<Categoria> categorias = new ArrayList<>();
 		
 	@Valid
