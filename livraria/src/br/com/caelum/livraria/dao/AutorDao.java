42faff4df3df94fdbc86ad1bf9b3c65d74b0715b
Marcio Sousa <sotreze@yahoo.com.br>
2019-07-10 23:00:49 -0300

    Ciclo de vida dos Session Beans

--- livraria/src/br/com/caelum/livraria/dao/AutorDao.java
+++ livraria/src/br/com/caelum/livraria/dao/AutorDao.java
@@ -2,17 +2,36 @@ package br.com.caelum.livraria.dao;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 
 import br.com.caelum.livraria.modelo.Autor;
 
 @Stateless
 public class AutorDao {
 
-	private Banco banco = new Banco();
+	@Inject
+	private Banco banco;
+	
+	@PostConstruct
+	void aposCriacao() {
+		System.out.println("Autor foi criado");
+	}
 
 	public void salva(Autor autor) {
+		
+		System.out.println("salvando Autor " + autor.getNome());
+		
+//		try {
+//			Thread.sleep(20000); //20s
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+		
 		banco.save(autor);
+		
+		System.out.println("salvou Autor " + autor.getNome());
 	}
 	
 	public List<Autor> todosAutores() {
