136c198e5efd74dcb6f6f219ad0cf8e58719f060
Marcio Sousa <sotreze@yahoo.com.br>
2019-07-16 15:36:42 -0300

    HOME -  Gerenciamento de Transações com JTA

--- livraria/src/br/com/caelum/livraria/dao/AutorDao.java
+++ livraria/src/br/com/caelum/livraria/dao/AutorDao.java
@@ -4,34 +4,45 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
 
 import br.com.caelum.livraria.modelo.Autor;
 
 @Stateless
+@TransactionManagement(TransactionManagementType.BEAN)
 public class AutorDao {
 
 	@PersistenceContext
 	private EntityManager manager;
+	
+	@Inject
+	UserTransaction tx;
 	
 	@PostConstruct
 	void aposCriacao() {
 		System.out.println("Autor foi criado");
 	}
 
+	@TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
 	public void salva(Autor autor) {
 		
 		System.out.println("salvando Autor " + autor.getNome());
 		
-//		try {
-//			Thread.sleep(20000); //20s
-//		} catch (InterruptedException e) {
-//			e.printStackTrace();
-//		}
-		
-		manager.persist(autor);
-		
+		try {
+			tx.begin();
+			manager.persist(autor);
+			tx.commit();
+	
+		} catch (Exception e) {
+			e.printStackTrace();
+		}	
 		System.out.println("salvou Autor " + autor.getNome());
 	}
 	
