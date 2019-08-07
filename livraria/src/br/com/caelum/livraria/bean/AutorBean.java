25b1f9c5b996f1644589171b19e51a3ccb620d9b
Marcio Jose Lima de Sousa <sotreze@yahoo.com.br>
2019-08-07 15:21:36 -0300

    CAMP - Desafio: CRUD do Autor

--- livraria/src/br/com/caelum/livraria/bean/AutorBean.java
+++ livraria/src/br/com/caelum/livraria/bean/AutorBean.java
@@ -1,19 +1,27 @@
 package br.com.caelum.livraria.bean;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 import br.com.caelum.livraria.dao.DAO;
 import br.com.caelum.livraria.modelo.Autor;
 import br.com.caelum.livraria.util.ForwardView;
-import br.com.caelum.livraria.util.RedirectView;
+//import br.com.caelum.livraria.util.RedirectView;
 
 @ManagedBean
+@ViewScoped
 public class AutorBean {
 
 	private Autor autor = new Autor();
 
 	public Autor getAutor() {
 		return autor;
+	}
+	
+	public List<Autor> getAutores() {
+		return new DAO<Autor>(Autor.class).listaTodos();
 	}
 
 	/*public String gravar() {
