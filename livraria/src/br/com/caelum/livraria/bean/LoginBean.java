801000150bafaa0922c4b021be2a890c7353dd1a
Marcio Jose Lima de Sousa <sotreze@yahoo.com.br>
2019-08-19 15:02:09 -0300

    CAMP - Autorização pelo PhaseListener

--- livraria/src/br/com/caelum/livraria/bean/LoginBean.java
+++ livraria/src/br/com/caelum/livraria/bean/LoginBean.java
@@ -2,6 +2,7 @@ package br.com.caelum.livraria.bean;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 import br.com.caelum.livraria.dao.UsuarioDao;
 import br.com.caelum.livraria.modelo.Usuario;
@@ -17,10 +18,12 @@ public class LoginBean {
 	}
 	
 	public String efetuaLogin() {
-		System.out.println("fazendo o login do usuário " + this.usuario.getEmail());	
+		System.out.println("fazendo o login do usuï¿½rio " + this.usuario.getEmail());	
 		
 		boolean existe = new UsuarioDao().existe(this.usuario);
 		if(existe) {
+			FacesContext context = FacesContext.getCurrentInstance();
+			context.getExternalContext().getSessionMap().put("usuarioLogado", this.usuario);
 			return "livro?faces-redirect=true";
 		}	
 		
