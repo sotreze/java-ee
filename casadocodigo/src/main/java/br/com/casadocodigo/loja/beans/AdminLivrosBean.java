0223adc6c9808729fde0a8e71df1da9be9d6e410
Marcio Sousa <sotreze@yahoo.com.br>
2019-11-05 15:27:37 -0300

    HOME - Os dados estão sumindo e concluindo o form

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
@@ -1,5 +1,28 @@
 package br.com.casadocodigo.loja.beans;
 
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+import br.com.casadocodigo.loja.models.Livro;
+
+// CDI
+
+@Named
+@RequestScoped
 public class AdminLivrosBean {
+	
+	private Livro livro = new Livro();
+
+	public void salvar() {
+		System.out.println("Livro cadastrado: " + livro);
+    }
+
+	public Livro getLivro() {
+		return livro;
+	}
+
+	public void setLivro(Livro livro) {
+		this.livro = livro;
+	}
 
 }
