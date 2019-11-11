ac0c2f5d2f9644bc82fba21370e61ccb85e946b0
Marcio Jose Lima de Sousa <sotreze@yahoo.com.br>
2019-11-11 14:59:13 -0300

    CAMP - Inserindo os Dados no Banco de Dados

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
@@ -1,8 +1,11 @@
 package br.com.casadocodigo.loja.beans;
 
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+import javax.transaction.Transactional;
 
+import br.com.casadocodigo.loja.daos.LivroDao;
 import br.com.casadocodigo.loja.models.Livro;
 
 // CDI
@@ -12,8 +15,14 @@ import br.com.casadocodigo.loja.models.Livro;
 public class AdminLivrosBean {
 	
 	private Livro livro = new Livro();
+	
+	// Context and Dependency injection
+	@Inject
+	private LivroDao dao;
 
+	@Transactional
 	public void salvar() {
+		dao.salvar(livro);
 		System.out.println("Livro cadastrado: " + livro);
     }
 
